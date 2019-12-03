2a6793b0c7e41a487e85e63e33ae3070e58fe440
Xleine <919143384@qq.com>
2019-12-03 12:21:54 +0800

    add 添加2048逻辑框架

---
+++ index.js
@@ -0,0 +1,48 @@
+var tiles = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+]
+var bestScore = 0
+var currentScore = 0
+var prevTile = undefined
+var nextTile = undefined
+
+var _2048 = {
+    chessboard: tiles,
+    bestScore: bestScore,
+    currentScore: currentScore,
+    prevTile: prevTile,
+    nextTile: nextTile,
+    move: function move(direction) {
+
+    },
+    merge: function merge() {
+
+    },
+    isWin: function isWin() {
+
+    },
+    isCanMove: function isCanWin() {
+
+    },
+    update: function update() {
+
+    },
+    newGame: function newGame() {
+
+    },
+    changeChessboard: function changeChessboard() {
+
+    },
+    createNextTile: function createNextTile() {
+
+    },
+    updatePrevAndNextTile: function updatePrevAndNextTile() {
+
+    },
+    isGameOver: function isGameOver() {
+
+    },
+}
