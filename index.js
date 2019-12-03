79717feff155c59eecc6754c6c09ec55ec826d94
Xleine <919143384@qq.com>
2019-12-03 21:13:59 +0800

    change 完成newGame函数

--- index.js
+++ index.js
@@ -44,7 +44,25 @@ var _2048 = {
         }
     },
     newGame: function newGame() {
+        //将html所有的className重置为初始值
+        for (let i = 0; i < chessboard.length; i++) {
+            for (let j = 0; j < chessboard.length; j++) {
+                const tileSpan = cells[i*4 + j].querySelector('span')
 
+                tileSpan.className = 'tile'
+            }
+        }
+        //将所有chessboard元素填充为0
+        this.chessboard.map( tiles => tiles.fill(0))
+
+        //获取nextTile信息
+        const [x, y, level] = this.createNextTile()
+        //设置nextTile相关属性
+        this.nextTile = [x, y]
+        this.chessboard[x][y] = level
+
+        //执行更新
+        this.update()
     },
     changeChessboard: function changeChessboard() {
 
