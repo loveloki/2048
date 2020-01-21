c3e75ed4dc4328d3e8d1a05477036c9facb41154
Xleine <919143384@qq.com>
2020-01-21 11:13:34 +0800

    add 添加单独的game.js，将游戏逻辑抽离

---
+++ game.js
@@ -0,0 +1,21 @@
+const Game = {
+  score: 0,
+  bestScore: 0,
+  init: function () {
+
+  },
+  update: function () {
+
+  },
+  isOver: function () {
+
+  },
+  start: function () {
+
+  },
+  restart: function () {
+
+  },
+}
+
+export default Game
