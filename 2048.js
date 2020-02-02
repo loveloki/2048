e8db051fb4cac7be7278254529ec82c27cdc0f87
Xleine <919143384@qq.com>
2020-02-03 01:54:31 +0800

    update 更改辅助函数调用方式，添加新的更改tile的level的辅助函数

--- 2048.js
+++ 2048.js
@@ -8,19 +8,22 @@ const cells = document.querySelectorAll('.cell')
 
 var _2048 = {
 	tool: {
-		showTilesList: function () {
+		showTilesList: function (_2048) {
 			//temp
 			let showList = []
-			for (let i = 0; i < this.gridLength; i++) {
+			for (let i = 0; i < _2048.gridLength; i++) {
 				let line = []
-				for (let j = 0; j < this.gridLength; j++) {
-					const level = this.chessboard[i][j].level
+				for (let j = 0; j < _2048.gridLength; j++) {
+					const level = _2048.chessboard[i][j].level
 					line.push(level)
 				}
 				showList.push(line)
 			}
 			console.table(showList)
 		},
+		changeTilesList: function (_2048, positionX, positionY, level) {
+			_2048.chessboard[positionX][positionY].level = level
+		}
 	},
 	chessboard: [],
 	gridLength: 4,
